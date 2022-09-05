e5fe37e7aef2cdd237a3251d7e148591fa1d4526
木荣 <372638156@qq.com>
2022-09-05 09:49:36 +0800

    feat: prerelease tag

--- src/core.rs
+++ src/core.rs
@@ -1,5 +1,7 @@
+use regex::Regex;
+
 use crate::{
-    constants::{LOOSE_RE, MAX_LENGTH},
+    constants::{self, LOOSE_RE, MAX_LENGTH},
     valid,
 };
 
@@ -7,6 +9,7 @@ pub struct Vern {
     pub major: i32,
     pub minor: i32,
     pub patch: i32,
+    pub prerelease: Vec<String>,
 }
 
 impl Vern {
@@ -18,6 +21,7 @@ impl Vern {
             major: 0,
             minor: 0,
             patch: 0,
+            prerelease: Vec::new(),
         };
 
         for cap in LOOSE_RE.captures_iter(version) {
@@ -30,6 +34,22 @@ impl Vern {
             let patch = cap[3].parse::<i32>().unwrap();
             assert!(patch <= MAX_LENGTH);
             vern.patch = patch;
+
+            if cap.len() > 5 {
+                if !cap.get(4).map_or("", |m| m.as_str()).is_empty() {
+                    let prerelease_arr = cap[4].split(".");
+                    for release in prerelease_arr {
+                        if Regex::new(r"^[0-9]+$").unwrap().is_match(release) {
+                            let num = release.parse::<i32>().unwrap();
+                            if num >= 0 && num < constants::MAX_LENGTH {
+                                vern.prerelease.push(release.to_owned());
+                            }
+                        } else {
+                            vern.prerelease.push(release.to_owned());
+                        }
+                    }
+                }
+            }
         }
         vern
     }
--- tests/basic.rs
+++ tests/basic.rs
@@ -2,9 +2,13 @@ use vern::core::Vern;
 
 #[test]
 fn test_basic_function() {
-    let version = "0.1.2";
+    let version = "0.1.2-re.1.z266";
     let vern = Vern::new(version);
     assert_eq!(vern.major, 0);
     assert_eq!(vern.minor, 1);
     assert_eq!(vern.patch, 2);
+    assert_eq!(vern.prerelease.contains(&"re".to_string()), true);
+    assert_eq!(vern.prerelease.contains(&"1".to_string()), true);
+    assert_eq!(vern.prerelease.contains(&"z266".to_string()), true);
+    assert_eq!(vern.prerelease.contains(&"266".to_string()), false);
 }
